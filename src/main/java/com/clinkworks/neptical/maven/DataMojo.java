6c0c482418339254d8f44e3bf7b29225ceb4a530
Anthony Clink <anthonyjclink@gmail.com>
2014-04-18 09:01:06 -0700

    hello maven!

--- src/main/java/com/clinkworks/neptical/maven/DataMojo.java
+++ src/main/java/com/clinkworks/neptical/maven/DataMojo.java
@@ -5,10 +5,14 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 
+import com.clinkworks.neptical.Data;
+import com.clinkworks.neptical.DataRegistry;
+
 @Mojo( name = "data")
 public class DataMojo extends AbstractMojo{
 
 	public void execute() throws MojoExecutionException, MojoFailureException {
+		Data data = DataRegistry.loadData();
 		System.out.println("\n\n\n ----- DATA MODULE EXECUTING ------ \n\n\n");
 	}
 
